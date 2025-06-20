df1c00837c78f37f15cf87298b21fb5f0c7e5f98
brown2882 <markbrown282828@gmail.com>
2025-06-20 22:18:59 +0400

    firstPage

--- src/component/header.js
+++ src/component/header.js
@@ -6,6 +6,14 @@ import Man from "../assets/image/man.jpg"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPen } from '@fortawesome/free-solid-svg-icons';
+import {faStore}  from '@fortawesome/free-solid-svg-icons';
+import {faDesktop}  from '@fortawesome/free-solid-svg-icons';
+import {faChartSimple}  from '@fortawesome/free-solid-svg-icons';
+import {faHeart}  from '@fortawesome/free-solid-svg-icons';
+import {faClock}  from '@fortawesome/free-solid-svg-icons';
+import {faMugSaucer}  from '@fortawesome/free-solid-svg-icons';
+import {faStar}  from '@fortawesome/free-solid-svg-icons';
+
 
 
 export function Header () {
@@ -62,7 +70,7 @@ export function Header () {
 
                 <h2 className="h22">What I Do</h2>
                 <hr className="hr"/>
-                <div>
+                <div className="globalIconDiv">
                   <div className="fapenDiv">
                   
                       <FontAwesomeIcon icon={faPen}  className="faPenicon" style={{ color: "#74C0FC",}} />
@@ -71,8 +79,115 @@ export function Header () {
                          <p>Mauris neque libero, aliquet vel mollis nec, euismod sed tellus. Mauris convallis dictum elit id volutpat. Vivamus blandit, dolor vitae lacinia maximus, risus velit vehicula odio, a tincidunt turpis turpis tempus ex</p>
                       </div>
                   </div>
+                  <div className="fapenDiv">
+                    
+                    <FontAwesomeIcon icon={faStore} className="faPenicon" style={{color: "#74C0FC",}} />
+                    <div>
+                        <h2>Ecommerce</h2>
+                        <p>Mauris neque libero, aliquet vel mollis nec, euismod sed tellus. Mauris convallis dictum elit id volutpat. Vivamus blandit, dolor vitae lacinia maximus, risus velit vehicula odio, a tincidunt turpis turpis tempus ex.</p>
+                    </div>
+                  </div>
 
                 </div>  
+
+                {/* //////////////////////////// */}
+
+                 <div className="globalIconDivTwo">
+                  <div className="fapenDiv">
+                  
+                      <FontAwesomeIcon icon={faDesktop} className="faPenicon" style={{color: "#74C0FC",}} />
+
+                      <div>
+                         <h2>Web Design</h2>
+                         <p>Mauris neque libero, aliquet vel mollis nec, euismod sed tellus. Mauris convallis dictum elit id volutpat. Vivamus blandit, dolor vitae lacinia maximus, risus velit vehicula odio, a tincidunt turpis turpis tempus ex</p>
+                      </div>
+                  </div>
+                  <div className="fapenDiv">
+                    
+                    <FontAwesomeIcon icon={faChartSimple} className="faPenicon" style={{color: "#74C0FC",}} />
+                    <div>
+                        <h2>Marketing</h2>
+                        <p>Mauris neque libero, aliquet vel mollis nec, euismod sed tellus. Mauris convallis dictum elit id volutpat. Vivamus blandit, dolor vitae lacinia maximus, risus velit vehicula odio, a tincidunt turpis turpis tempus ex.</p>
+                    </div>
+                  </div>
+
+                </div> 
+
+
+                
+                  <h2 className="h22">Fun Facts</h2>
+                  <hr className="hr"/>
+
+                <div className="funDiv">
+                  <div className="funDivOne">
+                      <FontAwesomeIcon icon={faHeart} className="funDivIcon" style={{color: "#74C0FC",}} />
+                      <h3>Happy Clients</h3>
+                      <h1>578</h1>
+                  </div>
+                  
+                   <div className="funDivOne">
+                      <FontAwesomeIcon icon={faClock} className="funDivIcon" style={{color: "#74C0FC",}} />
+                      <h3>Working Hours</h3>
+                      <h1>4,780</h1>
+                  </div>
+                  
+                  <div className="funDivOne">
+                      <FontAwesomeIcon icon={faStar} className="funDivIcon" style={{color: "#74C0FC",}} />
+                      <h3>Awards Won</h3>
+                      <h1>15</h1>
+                  </div>
+                 
+                  <div className="funDivOne">
+                      <FontAwesomeIcon icon={faMugSaucer} className="funDivIcon" style={{color: "#74C0FC",}} />
+                      <h3>Coffee Consumed</h3>
+                      <h1>1,286</h1>
+                  </div>
+                </div>
+
+                <h2 className="h22">Pricing</h2>
+                  <hr className="hr"/>
+                <div className="PricingDiv">
+                  <div className="priceDivOne">
+                        <p className="kk">Silver</p>
+                        <h2 className="k">$64</h2>
+                        <p className="kk">per mounth</p>
+                        <button className="bgtnTree">Contact</button>
+                        
+                        <p className="k">Lorem ipsum dolor</p>
+                        <p className="k">Pellentesque scelerisque</p>
+                        <p className="k">Morbi eu sagittis</p>
+                  </div>
+
+                    <div className="priceDivTwo">
+                        <p className="kk">Gold</p>
+                        <h2 className="k">$128</h2>
+                        <p className="kk">per mounth</p>
+                        <button className="bgtnFour">Contact</button>
+                        
+                        <p className="k">Lorem ipsum dolor</p>
+                        <p className="k">Pellentesque scelerisque</p>
+                        <p className="k">Morbi eu sagittis</p>
+                        <p className="k">Donec non diam</p>
+                  </div>
+
+                   <div className="priceDivTree">
+                        <p className="kk">Platinum</p>
+                        <h2 className="k">$256</h2>
+                        <p className="kk">per mounth</p>
+                        <button className="bgtnFour">Contact</button>
+                        
+                        <p className="k">Lorem ipsum dolor</p>
+                        <p className="k">Pellentesque scelerisque</p>
+                        <p className="k">Morbi eu sagittis</p>
+                        <p className="k">Donec non diam</p>
+                        <p className="k">Aenean nec libero</p>
+                  </div>
+                </div>
+
+                <hr className="hrr"/>
+
+                <p className="p20">© 2020 All rights reserved. LMPixels.</p>
+                  
             </nav>
 
            
